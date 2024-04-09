e4355be3c416f3291517c6a861b38fe2172d20fa
liyansheng <1761724207@qq.com>
2024-04-10 01:29:01 +0800

    移除daovoice

--- scripts/my_inject.js
+++ scripts/my_inject.js
@@ -7,6 +7,5 @@
  * @LastEditTime: 2023-03-18 19:47:30
  */
 hexo.extend.filter.register('theme_inject', function(injects) {
-    injects.head.file('default2', 'scripts/myDaoVoice.ejs', { key: 'value' }, -1);
-    injects.head.file('baiduStatics', 'scripts/baidu_statics.ejs', { key: 'value' }, -2);
+    injects.head.file('baiduStatics', 'scripts/baidu_statics.ejs', { key: 'value' }, -1);
 });
